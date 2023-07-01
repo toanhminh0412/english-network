4203f6e9bb4e176736f42065a71a20c94e23ad58
Archie To <toanhminh0412@gmail.com>
2023-07-01 14:33:53 -0700

    Remove a comment

--- src/components/NetworkGraph.js
+++ src/components/NetworkGraph.js
@@ -119,15 +119,6 @@ export default function NetworkGraph({width=1000, height=500}) {
     return (
     <>
         <svg viewBox={`0 0 ${width} ${height}`}></svg>
-        {/* <dialog id="bio-0" className="modal">
-            <form method="dialog" className="modal-box">
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click the button below to close</p>
-                <div className="modal-action">
-                    <button className="btn">Close</button>
-                </div>
-            </form>
-        </dialog> */}
         {people.map(person => (
         <dialog key={person.id} id={`bio-${person.id}`} className="modal">
             <form method="dialog" className="modal-box">
