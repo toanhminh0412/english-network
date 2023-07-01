2a4f19f146aea13e162d5f7de76efe456e656904
Archie To <toanhminh0412@gmail.com>
2023-07-01 15:16:25 -0700

    Add an upper nav bar

---
+++ src/components/UpperNav.js
@@ -0,0 +1,7 @@
+export default function UpperNav() {
+    return (
+    <div className="bg-white shadow-lg border-b-2 border-slate-100 py-4 min-h-[24px] fixed top-0 left-0 w-full flex flex-row">
+        <h1 className="font-semibold text-xl pl-4">EnglishNetwork</h1>
+    </div>
+    )
+}
